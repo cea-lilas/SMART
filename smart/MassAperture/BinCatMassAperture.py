Fix noise map calculation

smart/MassAperture/BinCatMassAperture.py
@@ -98,17 +98,15 @@ class BinCatMassAperture(MassApertureMap):
             gamma1_j_sq = gamma1_j**2
             gamma2_j_sq = gamma2_j**2
 
-        avg_weight, avg_weight_sq = 1, 1
+        avg_weight = 1
         if self._sum:
             weights_j = neighbor_data[:, 4]
             # the aperture size is mesured in non-empty pixels
             apr_size_pix = np.sum(neighbor_lenghts > 0)
             avg_weight = np.sum(weights_j) / apr_size_pix
-            if self._squares:
-                avg_weight_sq = np.sum(weights_j**2) / apr_size_pix
             
         center = (ra[i], dec[i])
-        return center, ra_j, dec_j, gamma1_j, gamma2_j, gamma1_j_sq, gamma2_j_sq, avg_weight, avg_weight_sq
+        return center, ra_j, dec_j, gamma1_j, gamma2_j, gamma1_j_sq, gamma2_j_sq, avg_weight
 
     def initialise_mass_aperture(
             self,

smart/MassAperture/FullCatMassAperture.py
@@ -28,15 +28,13 @@ class FullCatMassAperture(MassApertureMap):
             g1_j_sq = g1_j**2
             g2_j_sq = g2_j**2
 
-        avg_weight, avg_weight_sq = 1, 1
+        avg_weight = 1
         if self._sum and len(neighbors) > 0:
             apt_size_pix = np.sum(self.mask[hp.query_disc(self.nside, vecs[:, i], radius_rad)])
             if apt_size_pix > 0:
                 avg_weight = np.sum(shear_catalog.weight[neighbors])/apt_size_pix
-                if self._squares:
-                    avg_weight_sq = np.sum(shear_catalog.weight[neighbors]**2)/apt_size_pix
 
-        return center, ra_j, dec_j, g1_j, g2_j, g1_j_sq, g2_j_sq, avg_weight, avg_weight_sq
+        return center, ra_j, dec_j, g1_j, g2_j, g1_j_sq, g2_j_sq, avg_weight
 
     def initialise_mass_aperture(
             self,

smart/MassAperture/MassAperture.py
@@ -152,7 +152,7 @@ class MassApertureMap(ABC):
             **kwargs :
                 Additional arguments to pass to the query_neighbors function, depending on the mass aperture implementation.
         """
-        center, ra, dec, gamma1, gamma2, gamma1_sq, gamma2_sq, avg_weight, avg_weight_sq = self.query_neighbors(
+        center, ra, dec, gamma1, gamma2, gamma1_sq, gamma2_sq, avg_weight = self.query_neighbors(
             i, vecs, radius_rad, **kwargs)
 
         center_ra, center_dec = center
@@ -180,7 +180,7 @@ class MassApertureMap(ABC):
         if not self._squares:
             return mapE_i, mapB_i
 
-        map_vnoise_i = 1 / 2 * np.sum((gamma1_sq + gamma2_sq) * Q**2)/avg_weight_sq
+        map_vnoise_i = 1 / 2 * np.sum((gamma1_sq + gamma2_sq) * Q**2)/avg_weight**2
         return mapE_i, mapB_i, map_vnoise_i
 
     def get_healpix_ra_dec(self):

smart/MassAperture/PixelMassAperture.py
@@ -171,15 +171,13 @@ class PixelMassAperture(MassApertureMap):
             gamma1_sq_j = None
             gamma2_sq_j = None
 
-        avg_weight, avg_weight_sq = 1, 1
+        avg_weight = 1
         if self._sum:
             non_zero_weights = self.ng_weight[neighbors][self.ng_weight[neighbors] > 0]
             if len(non_zero_weights) > 0:
                 avg_weight = np.mean(non_zero_weights)
-                if self._squares:
-                    avg_weight_sq = np.mean(non_zero_weights**2)
 
-        return center, ra_j, dec_j, gamma1_j, gamma2_j, gamma1_sq_j, gamma2_sq_j, avg_weight, avg_weight_sq
+        return center, ra_j, dec_j, gamma1_j, gamma2_j, gamma1_sq_j, gamma2_sq_j, avg_weight
 
     def initialise_mass_aperture(
             self,